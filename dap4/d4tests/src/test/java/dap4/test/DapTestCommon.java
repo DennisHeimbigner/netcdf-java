9a082cadc5b708afdc8ece7c3046daf6a29cea7f
Dennis Heimbigner <dmh@ucar.edu>
2022-09-30 18:55:18 -0600

    deps2

--- dap4/d4tests/src/test/java/dap4/test/DapTestCommon.java
+++ dap4/d4tests/src/test/java/dap4/test/DapTestCommon.java
@@ -218,19 +218,21 @@ abstract public class DapTestCommon extends UnitTestCommon {
     return this.dap4resourcedir;
   }
 
-  /* static protected void testSetup() {
-    try {
-      // Always prefer Nc4Iosp over HDF5
-      NetcdfFile.iospDeRegister(NC4IOSP);
-      NetcdfFile.registerIOProviderPreferred(NC4IOSP, ucar.nc2.iosp.hdf5.H5iosp.class);
-      // Print out the library version
-      System.err.printf("Netcdf-c library version: %s%n", getCLibraryVersion());
-      System.err.flush();
-    } catch (Exception e) {
-      System.err.println("Cannot load ucar.nc2.jni.netcdf.Nc4Iosp");
-    }
-  }
-*/
+  /*
+   *  static protected void testSetup() {
+   *    try {
+   *      // Always prefer Nc4Iosp over HDF5
+   *      NetcdfFile.iospDeRegister(NC4IOSP);
+   *      NetcdfFile.registerIOProviderPreferred(NC4IOSP, ucar.nc2.iosp.hdf5.H5iosp.class);
+   *      // Print out the library version
+   *      System.err.printf("Netcdf-c library version: %s%n", getCLibraryVersion());
+   *      System.err.flush();
+   *    } catch (Exception e) {
+   *      System.err.println("Cannot load ucar.nc2.jni.netcdf.Nc4Iosp");
+   *    }
+   *  }
+   */
+
   static void printDir(String path) {
     File testdirf = new File(path);
     assert (testdirf.canRead());
@@ -245,21 +247,21 @@ abstract public class DapTestCommon extends UnitTestCommon {
     System.err.println("*******************");
     System.err.flush();
   }
-/*
-  static public String getCLibraryVersion() {
-    Nc4prototypes nc4 = getCLibrary();
-    return (nc4 == null ? "Unknown" : nc4.nc_inq_libvers());
-  }
 
-  static public Nc4prototypes getCLibrary() {
-    try {
-      Method getclib = NC4IOSP.getMethod("getCLibrary");
-      return (Nc4prototypes) getclib.invoke(null);
-    } catch (NoSuchMethodException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-      return null;
-    }
-  }
-  b
- */
+  /*
+   *  static public String getCLibraryVersion() {
+   *    Nc4prototypes nc4 = getCLibrary();
+   *    return (nc4 == null ? "Unknown" : nc4.nc_inq_libvers());
+   *  }
+   *
+   *  static public Nc4prototypes getCLibrary() {
+   *    try {
+   *      Method getclib = NC4IOSP.getMethod("getCLibrary");
+   *      return (Nc4prototypes) getclib.invoke(null);
+   *    } catch (NoSuchMethodException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+   *      return null;
+   *    }
+   *  }
+   */
 }
 
