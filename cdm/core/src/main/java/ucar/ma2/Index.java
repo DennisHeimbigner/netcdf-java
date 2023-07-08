41472a20165f3b195875ef5371ef9af6eef15843
Dennis Heimbigner <dmh@ucar.edu>
2023-07-08 13:18:57 -0600

    final

--- cdm/core/src/main/java/ucar/ma2/Index.java
+++ cdm/core/src/main/java/ucar/ma2/Index.java
@@ -570,11 +570,7 @@ public class Index implements Cloneable {
       throw new ArrayIndexOutOfBoundsException();
     if (rank == 0)
       return this;
-    int prefixrank = -1;
-    if(hasvlen)
-        prefixrank = rank - 1;
-    else
-        prefixrank = rank;
+    int prefixrank = (hasvlen ? rank - 1 : rank);
     System.arraycopy(index, 0, current, 0, prefixrank);
     if (hasvlen)
       current[rank] = -1; // ??
